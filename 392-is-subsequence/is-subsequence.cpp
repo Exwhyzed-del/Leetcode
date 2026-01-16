95b0a9f28d1e5dae0aa26273275c7e360d26351e
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-01-16 10:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.29%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,12 +3,12 @@ public:
     bool isSubsequence(string s, string t) {
         int i=0;
         int j=0;
-        while(i<s.size() && j<t.size()){
+        while(i<s.length() &&  j<t.length()){
             if(s[i]==t[j]){
                 i++;
             }
             j++;
         }
-        return i==s.size();
+        return i==s.length();
     }
 };
