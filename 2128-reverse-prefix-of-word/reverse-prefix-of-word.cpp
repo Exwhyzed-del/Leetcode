fdc1938f7a7b02d41b6e5253593106bc8f5edaf1
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-01-30 16:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (87.59%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,27 +1,41 @@
+// class Solution {
+// public:
+//     string reversePrefix(string word, char ch) {
+//         int idx = -1;
+
+//         // 1. Find first occurrence of ch
+//         for (int i = 0; i < word.size(); i++) {
+//             if (word[i] == ch) {
+//                 idx = i;
+//                 break;
+//             }
+//         }
+
+//         // 2. If ch not found, return original word
+//         if (idx == -1) return word;
+
+//         // 3. Reverse prefix [0..idx]
+//         int s = 0, e = idx;
+//         while (s < e) {
+//             swap(word[s], word[e]);
+//             s++;
+//             e--;
+//         }
+
+//         return word;
+        
+//     }
+// };
+
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int idx = -1;
+        int idx = word.find(ch);   // find first occurrence
 
-        // 1. Find first occurrence of ch
-        for (int i = 0; i < word.size(); i++) {
-            if (word[i] == ch) {
-                idx = i;
-                break;
-            }
-        }
+        if (idx == string::npos)
+            return word;
 
-        // 2. If ch not found, return original word
-        if (idx == -1) return word;
-
-        // 3. Reverse prefix [0..idx]
-        int s = 0, e = idx;
-        while (s < e) {
-            swap(word[s], word[e]);
-            s++;
-            e--;
-        }
-
+        reverse(word.begin(), word.begin() + idx + 1);
         return word;
     }
 };
