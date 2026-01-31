2176aca36ff7b4f6df19e1217caf80b1286d05fc
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-01-31 19:34:09 +0530

    Time: 3 ms (37.13%) | Memory: 10.3 MB (52.07%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,21 +1,56 @@
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         string word = s;
+//         int start = 0;
+//         int end = s.length() - 1;
+//         string vowels = "aeiouAEIOU";
+//         while (start < end) {
+//             while (start < end && vowels.find(word[start]) == string::npos) {
+//                 start++;
+//             }
+//             while (start < end && vowels.find(word[end]) == string::npos) {
+//                 end--;
+//             }
+//             swap(word[start], word[end]);
+//             start++;
+//             end--;
+//         }
+//         return word;
+//     }
+// };
+
 class Solution {
 public:
-    string reverseVowels(string s) {
-        string word = s;
-        int start = 0;
-        int end = s.length() - 1;
-        string vowels = "aeiouAEIOU";
-        while (start < end) {
-            while (start < end && vowels.find(word[start]) == string::npos) {
-                start++;
+
+    bool isLower(char c)
+    {
+        c=tolower(c);
+
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+    }
+    string reverseVowels(string s) 
+    {
+        int size=s.size();
+
+        int i=0,j=size-1;
+
+        while(i<j)
+        {
+            if(!isLower(s[i]))
+            {
+                i++;
             }
-            while (start < end && vowels.find(word[end]) == string::npos) {
-                end--;
+            else if(!isLower(s[j]))
+            {
+                j--;
             }
-            swap(word[start], word[end]);
-            start++;
-            end--;
+            else
+            {
+                swap(s[i],s[j]);
+                i++,j--;
+            }
         }
-        return word;
+        return s;
     }
 };
