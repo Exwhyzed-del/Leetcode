1f028bb550b75d293042e91242292fa781b939f9
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-02-01 19:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (91.52%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,54 +1,12 @@
-// class Solution {
-// public:
-//     string reverseVowels(string s) {
-//         string word = s;
-//         int start = 0;
-//         int end = s.length() - 1;
-//         string vowels = "aeiouAEIOU";
-//         while (start < end) {
-//             while (start < end && vowels.find(word[start]) == string::npos) {
-//                 start++;
-//             }
-//             while (start < end && vowels.find(word[end]) == string::npos) {
-//                 end--;
-//             }
-//             swap(word[start], word[end]);
-//             start++;
-//             end--;
-//         }
-//         return word;
-//     }
-// };
-
 class Solution {
 public:
-
-    bool isLower(char c)
-    {
-        c=tolower(c);
-
-        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
-    }
-    string reverseVowels(string s) 
-    {
-        int size=s.size();
-
-        int i=0,j=size-1;
-
-        while(i<j)
-        {
-            if(!isLower(s[i]))
-            {
-                i++;
-            }
-            else if(!isLower(s[j]))
-            {
-                j--;
-            }
-            else
-            {
-                swap(s[i],s[j]);
-                i++,j--;
+    string reverseVowels(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
             }
         }
         return s;
