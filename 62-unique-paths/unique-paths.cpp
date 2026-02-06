be3da7c24c7daae9dca95ce4341426610429a09f
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-02-06 11:49:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (52.07%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int>(n));
+
+        // last row
+        for(int j = 0; j < n; j++){
+            grid[m-1][j] = 1;
+        }
+
+        // last column
+        for(int i = 0; i < m; i++){
+            grid[i][n-1] = 1;
+        }
+
+        // fill rest of grid
+        for(int i = m-2; i >= 0; i--){
+            for(int j = n-2; j >= 0; j--){
+                grid[i][j] = grid[i+1][j] + grid[i][j+1];
+            }
+        }
+
+        return grid[0][0];
+    }
+};
