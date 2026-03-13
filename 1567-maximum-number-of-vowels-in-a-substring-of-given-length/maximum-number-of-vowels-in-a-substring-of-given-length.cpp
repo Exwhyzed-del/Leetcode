9c8b4a90b20e4559902307b4be91aff727f02639
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-03-13 11:03:56 +0530

    Time: 3 ms (87.98%) | Memory: 13 MB (98.91%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+     bool checkstring(char c){
+        return c=='a'||c=='i'||c=='e'||c=='o'||c=='u';
+     }
     int maxVowels(string s, int k) {
-        int n = s.size();
-        int j = 0 ;
-       int i = 0 ;
-       int ans = 0 ;
-       int count = 0 ;
-       while( i < n){
-        if(s[i] == 'a' ||s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u'){
-            count++;
-        }
-        i++;
-        if( i - j > k){ 
-            if(s[j] == 'a' ||s[j] == 'e' ||s[j] == 'i' ||s[j] == 'o' ||s[j] == 'u'){
-            count--;
-        }
-        j++;
+        int n=s.length();
+        int i=0;
+        int count=0,maxi=INT_MIN;
+        for(int j=0;j<n;j++){
+            if(checkstring(s[j])){
+                count++;
+            }
+            if(j-i+1==k){
+                maxi=max(count,maxi);
+
+                if(checkstring(s[i])){
+                    count--;
+                }
+                i++;
+            }
 
         }
-        if(i - j == k){
-        ans = max( ans , count );
-        }
-       }
-        return  ans;
+        return maxi;
+
+        
     }
 };
