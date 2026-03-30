9ee20c7725b84347679470e47bfdfa168738a6f0
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-03-30 14:46:04 +0530

    Time: 123 ms (5.02%) | Memory: 29.4 MB (9.87%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        
-        // Step 1: duplicate array
+
         vector<int> temp = nums;
         for(int i = 0; i < n; i++) {
             temp.push_back(nums[i]);
@@ -11,7 +10,7 @@ public:
 
         vector<int> ans(n, -1);
 
-        // Step 2: same logic as yours (just linear now)
+       
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < 2 * n; j++) {
                 if(temp[j] > nums[i]) {
