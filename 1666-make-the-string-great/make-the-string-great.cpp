b4136b42a21adc781ea90d5803caea5b9b18759b
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-04-03 10:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (96.50%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i=0;i<s.size();i++){
-            if(s[i]-s[i+1]==32 || s[i]-s[i+1]==-32){
-                s.erase(i,2);
-                i=-1;
+        string result = "";
 
+        for(char ch : s){
+            if(!result.empty() && abs(result.back() - ch) == 32){
+                result.pop_back();
+            } else {
+                result.push_back(ch);
             }
         }
-        return s;
+
+        return result;
     }
 };
