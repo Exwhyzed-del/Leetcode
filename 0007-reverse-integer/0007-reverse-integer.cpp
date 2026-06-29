df256e2b08f4e9a2e333bf59d3b2a3cdcb517a2a
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-06-29 10:11:14 +0530

    Time: 3 ms (29.39%), Space: 8.6 MB (54.3%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rem=0;
+        while(x!=0){
+            int lastdig=x%10;
+            if((rem > INT_MAX/10) || (rem < INT_MIN/10)){
+                return 0;
+            }
+            rem=(rem*10)+lastdig;
+            x=x/10;
+        }
+        return rem;
+    }
+};
