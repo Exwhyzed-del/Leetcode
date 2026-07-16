7ba05aed41b57554cbbafe8098a9f4ae24f3da88
Aryan Kumar Sharma <exwhyzed596@gmail.com>
2026-07-16 13:31:06 +0530

    Time: 205 ms (5.31%), Space: 56.3 MB (39.36%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-
         unordered_map<int,int> mp1, mp2;
         int l1 = 0, l2 = 0;
         int ans = 0;
 
         for (int r = 0; r < nums.size(); r++) {
 
+            // grow both windows by including nums[r]
             mp1[nums[r]]++;
             mp2[nums[r]]++;
 
+            // shrink mp1's window until it has at most k distinct values
             while (mp1.size() > k) {
                 mp1[nums[l1]]--;
                 if (mp1[nums[l1]] == 0)
@@ -18,6 +19,7 @@ public:
                 l1++;
             }
 
+            // shrink mp2's window until it has at most k-1 distinct values
             while (mp2.size() > k - 1) {
                 mp2[nums[l2]]--;
                 if (mp2[nums[l2]] == 0)
@@ -25,7 +27,10 @@ public:
                 l2++;
             }
 
-            ans += (l2 - l1);
+            // instead of ans += (l2 - l1), count each valid start one at a time
+            for (int start = l1; start < l2; start++) {
+                ans++;
+            }
         }
 
         return ans;
